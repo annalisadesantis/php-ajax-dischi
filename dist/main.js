ea1d1a86440700c1b2d8361472e32173c6533277
Annalisa <annalisadesantis770@gmail.com>
2020-12-17 16:13:38 +0100

    aggiunta commenti nel main

--- dist/main.js
+++ dist/main.js
@@ -10977,28 +10977,40 @@ return jQuery;
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
+// Importo l'installazione di jquery
 var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
 $(document).ready(function () {
-  var sorgente = $("#card-template").html();
-  var template = Handlebars.compile(sorgente);
+  // Inizializzo i passaggi handlebars
+  // dove si trova il template nel mio file html
+  var sorgente = $("#card-template").html(); // dico a handlebars di compilare il sorgente
+
+  var template = Handlebars.compile(sorgente); // chiamata ajax
+
   $.ajax({
+    // Il server che verrà chiamato
     url: "vers-ajax/../dischi.php",
+    // Metodo richiesta
     method: "GET",
+    // Chaiamata a buon fine
     success: function success(data) {
-      console.log(data);
+      console.log(data); // Ciclo l'array che mi viene fornito dalla chiamata ajax
 
       for (var i = 0; i < data.length; i++) {
+        // Salvo i dati ciclati
         var risultati = {
           "poster": data[i].poster,
           "title": data[i].title,
           "author": data[i].author,
           "year": data[i].year
-        };
-        var stampoRisultati = template(risultati);
+        }; // Salvo i dati ciclati in una variabile
+
+        var stampoRisultati = template(risultati); // Stampo in pagina i dati ricavati
+
         $(".container").append(stampoRisultati);
       }
     },
+    // In caso la chiamata ajax non vada a buon fine
     error: function error() {
       console.log("errore");
     }
--- src/main.js
+++ src/main.js
@@ -1,20 +1,30 @@
+// Importo l'installazione di jquery
 const $ = require( "jquery" );
 
 $(document).ready(function() {
 
+    // Inizializzo i passaggi handlebars
+    // dove si trova il template nel mio file html
     const sorgente = $("#card-template").html();
+    // dico a handlebars di compilare il sorgente
     const template = Handlebars.compile(sorgente);
 
+    // chiamata ajax
     $.ajax({
 
+        // Il server che verrà chiamato
         url: "vers-ajax/../dischi.php",
+        // Metodo richiesta
         method: "GET",
+        // Chaiamata a buon fine
         success: function(data){
 
             console.log(data);
 
+            // Ciclo l'array che mi viene fornito dalla chiamata ajax
             for (var i = 0; i < data.length; i++) {
 
+                // Salvo i dati ciclati
                 var risultati = {
                     "poster": data[i].poster,
                     "title": data[i].title,
@@ -22,10 +32,13 @@ $(document).ready(function() {
                     "year": data[i].year
                 };
 
+                // Salvo i dati ciclati in una variabile
                 var stampoRisultati = template(risultati);
+                // Stampo in pagina i dati ricavati
                 $(".container").append(stampoRisultati);
             }
         },
+        // In caso la chiamata ajax non vada a buon fine
         error: function(){
             console.log("errore");
         }
